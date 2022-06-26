6ae2a5c5933ebedd63ac3a0f9bed4f58fb129747
Rupendra0026 <rupendravnvss@gmail.com>
2022-06-26 22:39:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Mytodo from './Mytodo';
+import mytodo from './Mytodo';
+
+function App() {
+  return (
+    <div className="App contianer border border-primary rounded mt-5"
+    style={{background:'skyblue',
+    backgroundSize:'cover'}}>
+     {/* <h1>hello rupendra</h1>
+     <h5>hii</h5> */}
+     <Mytodo/>
+    </div>
+
+  );
+}
+
+export default App;
